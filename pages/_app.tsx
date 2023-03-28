a1e276651e984a58ab207c2ed50a44e275b1fe94
unclebigbay <ayodelesamueladebayo143@gmail.com>
2023-03-28 20:44:36 +0100

    Add and setup GA4 analytics Install firebase

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,47 @@ import "@/styles/globals.css";
 import { ToastProvider } from "@radix-ui/react-toast";
 
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import Script from "next/script";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      //@ts-ignore
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
-    <ToastProvider>
-      <Component {...pageProps} />
-    </ToastProvider>
+    <>
+      <Script
+        strategy='afterInteractive'
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA4_MEASUREMENT_ID}`}
+      ></Script>
+      <Script
+        id='google-analytics'
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GA4_MEASUREMENT_ID}', {
+          page_path: window.location.pathname,
+          });
+        `,
+        }}
+      />
+      <ToastProvider>
+        <Component {...pageProps} />
+      </ToastProvider>
+    </>
   );
 }
