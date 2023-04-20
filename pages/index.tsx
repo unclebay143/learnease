c3eb4451fbda1a99382d452ce64ae555ec3d215d
unclebigbay <ayodelesamueladebayo143@gmail.com>
2023-04-20 17:30:35 +0100

    remove testimonial from production

--- pages/index.tsx
+++ pages/index.tsx
@@ -224,7 +224,7 @@ export default function Home({ stars }: { stars: number }) {
         />
       )}
 
-      <Testimonials />
+      {/* <Testimonials /> */}
 
       <OSS stars={stars} />
     </HomeLayout>
