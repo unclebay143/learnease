b9f2aef69ce2810f6666c43f3d3bdc6fb7a20571
unclebigbay <ayodelesamueladebayo143@gmail.com>
2023-04-21 22:28:53 +0100

    Add og image

--- components/layouts/meta.tsx
+++ components/layouts/meta.tsx
@@ -2,10 +2,12 @@ import { FAVICON_FOLDER } from "@/lib/constants";
 import Head from "next/head";
 
 export default function Meta({
+  sitename = "LearnEase.vercel.app",
   title = "LearnEase - Unlock the Power of Learning with AI",
   description = "Your One-Stop Destination for Easy-to-Understand Tech Concepts with AI Assistance.",
-  image = "",
+  image = "https://learnease.vercel.app/_static/demo/og-gif.png",
 }: {
+  sitename?: string;
   title?: string;
   description?: string;
   image?: string;
@@ -13,31 +15,19 @@ export default function Meta({
   return (
     <Head>
       <title>{title}</title>
+      <link rel='icon' href={`${FAVICON_FOLDER}/favicon.ico`} />
       <meta name='description' content={description} />
 
-      <link
-        rel='icon'
-        type='image/png'
-        sizes='32x32'
-        href={`${FAVICON_FOLDER}/favicon.ico`}
-      />
-
-      <meta name='theme-color' content='#ffffff' />
-      <meta charSet='utf-8' />
-      <meta name='viewport' content='width=device-width, initial-scale=1' />
-      <meta itemProp='image' content={image} />
-
+      <meta property='og:site_name' content={sitename} />
       <meta property='og:title' content={title} />
       <meta property='og:description' content={description} />
       <meta property='og:image' content={image} />
 
+      <meta charSet='utf-8' />
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
+
       <meta name='twitter:card' content='summary_large_image' />
-      <meta name='twitter:site' content='https://learnease.vercel.app/' />
-      <meta name='twitter:creator' content='@unclebigbay143' />
-      <meta
-        name='twitter:title'
-        content='LearnEase - Your One-Stop Destination for Easy-to-Understand Tech Concepts with AI Assistance.'
-      />
+      <meta name='twitter:title' content={title} />
       <meta name='twitter:description' content={description} />
       <meta name='twitter:image' content={image} />
     </Head>
