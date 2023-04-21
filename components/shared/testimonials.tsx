53385677078244d8df8528ac4631ebff00f1e925
unclebigbay <ayodelesamueladebayo143@gmail.com>
2023-04-21 21:25:38 +0100

    modularize testimonial constants and add rapture testimony

--- components/shared/testimonials.tsx
+++ components/shared/testimonials.tsx
@@ -1,112 +1,10 @@
+import { srcIcon, testimonials } from "@/lib/constants/testimonials";
 import Image from "next/image";
 import React, { ReactElement } from "react";
 
 // credit: https://github.com/Nutlope/roomGPT/blob/main/components/Testimonials.tsx
 
 type Props = {};
-const testimonials = [
-  [
-    {
-      author: {
-        name: "Perspective",
-        img: "/_static/testimonial-headshots/perspective.webp",
-        title: "Software engineer",
-      },
-      quote:
-        "The UI is great and has an enjoyable feel to it. I'd definitely use this.",
-      srcUrl:
-        "https://twitter.com/iamthebuilder__/status/1648968287961722880?s=20",
-      src: "Twitter",
-    },
-    {
-      author: {
-        name: "Konadu Akwasi 👨‍💻",
-        img: "/_static/testimonial-headshots/konadu-akwasi.webp",
-        title: "Full-Stack Web Developer",
-      },
-      quote: "This looks cool, uncle BJay  🚀",
-      srcUrl:
-        "https://twitter.com/akuoko_konadu/status/1648982207170596864?s=20",
-      src: "Twitter",
-    },
-  ],
-  [
-    {
-      author: {
-        name: "Pyman",
-        title: "Python developer",
-        img: "/_static/testimonial-headshots/pyman.webp",
-      },
-      quote: "This amazing! The UI feel is lovely",
-      srcUrl:
-        "https://twitter.com/CodeShagbaor/status/1648994290893299713?s=20",
-      src: "Twitter",
-    },
-    {
-      author: {
-        name: "Chinenye Anikwenze",
-        title: "Technical Writer",
-        img: "/_static/testimonial-headshots/chinenye-anikwenze.jpeg",
-      },
-      quote: `Omg! This is really good!! I really love that you added analogy feature and also real world application of the concept.
-      Cos I have a bit of challenge with understanding how to implement concepts in real world projects`,
-      srcUrl:
-        "https://www.linkedin.com/feed/update/urn:li:activity:7054719570867011584?commentUrn=urn%3Ali%3Acomment%3A%28activity%3A7054719570867011584%2C7054737745067294720%29",
-      src: "LinkedIn",
-    },
-  ],
-  [
-    {
-      author: {
-        name: "Andrew Baisden ♐️",
-        title: "Software engineer",
-        img: "/_static/testimonial-headshots/andrew-baisden.webp",
-      },
-      quote: "Another worthwhile addition to my list.",
-      srcUrl:
-        "https://twitter.com/andrewbaisden/status/1647205352511885313?s=20",
-      src: "Twitter",
-    },
-    {
-      author: {
-        name: "LEO",
-        title: "Web developer",
-        img: "/_static/testimonial-headshots/leo.webp",
-      },
-      quote: "UncleBigBay, this is good 🔥",
-      srcUrl: "https://twitter.com/Omaebije/status/1649362907107414017?s=20",
-      src: "Twitter",
-    },
-  ],
-];
-
-const srcIcon: { [key: string]: ReactElement } = {
-  LinkedIn: (
-    <svg
-      xmlns='http://www.w3.org/2000/svg'
-      width={16}
-      height={16}
-      fill='currentColor'
-      className='inline-block ml-1 group-hover:text-green-600 bi bi-linkedin'
-      viewBox='0 0 16 16'
-    >
-      <path d='M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z' />
-    </svg>
-  ),
-
-  Twitter: (
-    <svg
-      xmlns='http://www.w3.org/2000/svg'
-      width={16}
-      height={16}
-      fill='currentColor'
-      className='inline-block ml-1 group-hover:text-green-600 bi bi-twitter'
-      viewBox='0 0 16 16'
-    >
-      <path d='M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z' />
-    </svg>
-  ),
-};
 
 const Testimonials = (props: Props) => {
   return (
@@ -146,49 +44,30 @@ const Testimonials = (props: Props) => {
             {testimonials.map((column, columnIndex) => (
               <li key={columnIndex}>
                 <ul role='list' className='flex flex-col gap-y-6 sm:gap-y-8'>
-                  {column.map((testimonial, testimonialIndex) => (
-                    <li
-                      key={testimonialIndex}
-                      className='transition duration-300 ease-in-out hover:scale-105 '
-                    >
-                      <a
-                        href={testimonial.srcUrl}
-                        target='_blank'
-                        rel='noreferrer'
+                  {column.map(
+                    ({ quote, author, src, srcUrl }, testimonialIndex) => (
+                      <li
+                        key={testimonialIndex}
+                        className='transition duration-300 ease-in-out hover:scale-105 '
                       >
-                        <figure className='relative p-6 shadow-xl group rounded-2xl bg-gray-50 shadow-slate-900/10'>
-                          <blockquote className='relative'>
-                            <p className='text-lg tracking-tight text-gray-600'>
-                              &quot;{testimonial.quote}&quot;
-                              {srcIcon[testimonial.src]}
-                            </p>
-                          </blockquote>
-                          <figcaption className='relative flex items-center justify-between pt-6 mt-6 border-t border-slate-100'>
-                            <div>
-                              <div className='justify-start text-base text-green-600 items- font-display'>
-                                {testimonial.author.name}
-                              </div>
-                              <div className='mt-1 text-sm text-gray-400 capitalize'>
-                                {testimonial.author.title}
-                              </div>
-                            </div>
-                            <section className='flex items-end'>
-                              <div className='p-1 overflow-hidden border border-green-400 rounded-full bg-green-50'>
-                                <Image
-                                  className='object-cover rounded-full h-14 w-14'
-                                  src={testimonial.author.img}
-                                  alt='picture of the testimonial author'
-                                  width={56}
-                                  height={56}
-                                />
-                              </div>
-                              {/* {srcIcon[testimonial.src]} */}
-                            </section>
-                          </figcaption>
-                        </figure>
-                      </a>
-                    </li>
-                  ))}
+                        {srcUrl ? (
+                          <a href={srcUrl} target={"_blank"} rel='noreferrer'>
+                            <TestimonialCard
+                              quote={quote}
+                              author={author}
+                              src={src}
+                            />
+                          </a>
+                        ) : (
+                          <TestimonialCard
+                            quote={quote}
+                            author={author}
+                            src={src}
+                          />
+                        )}
+                      </li>
+                    )
+                  )}
                 </ul>
               </li>
             ))}
@@ -196,6 +75,49 @@ const Testimonials = (props: Props) => {
         </div>
       </section>
     </div>
+  );
+};
+
+const TestimonialCard = ({
+  quote,
+  author,
+  src,
+}: {
+  quote: string;
+  author: { name: string; title: string; img: string };
+  src: string;
+}) => {
+  return (
+    <figure className='relative p-6 shadow-xl group rounded-2xl bg-gray-50 shadow-slate-900/10'>
+      <blockquote className='relative'>
+        <p className='text-lg tracking-tight text-gray-600'>
+          &quot;{quote}&quot;
+          {srcIcon[src]}
+        </p>
+      </blockquote>
+      <figcaption className='relative flex items-center justify-between pt-6 mt-6 border-t border-slate-100'>
+        <div>
+          <div className='justify-start text-base text-green-600 items- font-display'>
+            {author.name}
+          </div>
+          <div className='mt-1 text-sm text-gray-400 capitalize'>
+            {author.title}
+          </div>
+        </div>
+        <section className='flex items-end'>
+          <div className='p-1 overflow-hidden border border-green-400 rounded-full bg-green-50'>
+            <Image
+              className='object-cover rounded-full h-14 w-14'
+              src={author.img}
+              alt='picture of the testimonial author'
+              width={56}
+              height={56}
+            />
+          </div>
+          {/* {srcIcon[testimonial.src]} */}
+        </section>
+      </figcaption>
+    </figure>
   );
 };
 
